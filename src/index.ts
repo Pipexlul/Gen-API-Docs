e2c5292a1ab2da8d0b2d635005a1e4a4848d98d3
Pipexlul <felipgg@gmail.com>
2023-07-04 15:19:24 -0400

    Update index.ts to allow easy changing of listen port

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import server from "./server";
 
-server.listen(3000, () => {
-  console.log("Docs running on port 3000");
+const PORT = 3000;
+
+server.listen(PORT, () => {
+  console.log(`Docs running on port: ${PORT}`);
 });
