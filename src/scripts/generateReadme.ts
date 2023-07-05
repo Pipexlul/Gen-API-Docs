d4194825b2477a0ee26239f4c8c3d850f63360f9
Pipexlul <felipgg@gmail.com>
2023-07-05 00:48:21 -0400

    Update generateReadme.ts script to read directly from file in /dev/ folder

--- src/scripts/generateReadme.ts
+++ src/scripts/generateReadme.ts
@@ -3,76 +3,17 @@ import * as path from "path";
 
 import { version } from "../lib/version";
 
+type TokenToReplace = [string, string];
+
+const tokensToReplace: TokenToReplace[] = [["version", version]];
+
 const readmeFile = path.join(process.cwd(), "README.md");
-const readme = `# Gen API Docs v${version}
+const baseReadme = path.join(process.cwd(), "dev", "baseREADME.md");
 
-Small project to serve API docs of your project using express from a .json file that follows the OpenAPI specification, thanks to [swagger-ui-express](https://www.npmjs.com/package/swagger-ui-express).
+let readmeStr: string = fs.readFileSync(baseReadme, { encoding: "utf8" });
 
-And optionally convert a .yml file (that follows OpenAPI specification) to a .json format, so it can be used by the main feature of this project.
+for (const tokenToReplace of tokensToReplace) {
+  readmeStr = readmeStr.replace(`{${tokenToReplace[0]}}`, tokenToReplace[1]);
+}
 
-## Prerequisites
-
-Clone the repository
-
-\`\`\`bash
-git clone https://github.com/Pipexlul/Gen-API-Docs.git
-\`\`\`
-
-## Installation
-
-Install dependencies
-
-\`\`\`bash
-npm i
-\`\`\`
-
-## Usage
-
-Since this is a small project, there are some hardcoded values the code expects.
-
-1. If your API documentation file is in .json format
-  1. Rename your file to \`api.json\`
-  2. Place \`api.json\` inside the \`/data\` folder (Overwrite if needed)
-  3. Build Project
-  \`\`\`bash
-  npm run build
-  \`\`\`
-  4. Run server
-  \`\`\`bash
-  npm run start
-  \`\`\`
-  5. Go see your docs! By default they should be hosted at \`localhost:3000/api-docs\`
-2. If your API documentation file is in .yml format
-  1. Rename your file to \`apiContract.yml\`
-  2. Place \`apiContract.yml\` inside the \`/data\` folder (Overwrite if needed)
-  3. Run conversion script (This will build and run the necessary files)
-  \`\`\`bash
-  npm run convertYaml
-  \`\`\`
-  4. Build Project
-  \`\`\`bash
-  npm run build
-  \`\`\`
-  5. Run server
-  \`\`\`bash
-  npm run start
-  \`\`\`
-  6. Go see your docs! By default they should be hosted at \`localhost:3000/api-docs\`
-
-## Libraries used
-
-- [typescript](https://www.npmjs.com/package/typescript)
-- [express](https://www.npmjs.com/package/express)
-- [swagger-ui-express](https://www.npmjs.com/package/swagger-ui-express)
-- [read-js-yaml](https://www.npmjs.com/package/read-js-yaml)
-
-## Contributing
-
-Pull requests are welcome.
-
-## License
-
-[MIT](https://choosealicense.com/licenses/mit/)
-`;
-
-fs.writeFileSync(readmeFile, readme);
+fs.writeFileSync(readmeFile, readmeStr);
